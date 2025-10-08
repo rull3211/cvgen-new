5ac1d076d60983022262e8bab62730fe2c682912
Bence Daniel Szøke <54317680+rull3211@users.noreply.github.com>
2025-10-08 11:48:05 +0200

    styling: adding correct styling to collapsabletab

--- src/components/ClosableTab/ClosableTab.tsx
+++ src/components/ClosableTab/ClosableTab.tsx
@@ -8,7 +8,12 @@ export default function ClosableTab({header, children}:{header?:string | ReactNo
     const rotate = isOpen ? 180 : 0
     return(
         <Paper>
-            <Box component={"section"}><Typography>{header}</Typography><IconButton onClick={()=>setOpen(!isOpen)}><ExpandLessIcon sx={{transform: `rotate(${rotate}deg)`, transition:"transform ease 0.3s"}}/></IconButton></Box>
+            <Box sx={{
+                display: "flex",
+                justifyContent:"space-between",
+                padding: "0.5rem",
+                alignItems:"center"
+            }}  component={"section"}><Typography sx={{fontWeight:700, fontSize:"1.5rem"}}>{header}</Typography><IconButton onClick={()=>setOpen(!isOpen)}><ExpandLessIcon sx={{fontSize:"2rem", color:"black", transform: `rotate(${rotate}deg)`, transition:"transform ease 0.3s"}}/></IconButton></Box>
           
             <Box component={"section"} sx={{
                 maxHeight: isOpen ? "40rem" : "0px",
